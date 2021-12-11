dca1a0eed10d8639313d73831a51ab3fbcf32d27
shahrom322 <ololoev05@gmail.com>
2021-12-11 21:59:46 +0300

    Commit 0.2

--- conf/urls.py
+++ conf/urls.py
@@ -1,6 +1,9 @@
 import debug_toolbar
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from conf import settings
 
 
 urlpatterns = [
@@ -8,3 +11,6 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('', include('barber.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
