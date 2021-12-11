eead8284a80dabbd718fd297b473e48ad5b15875
AnastasiiaK120 <anastasiiakudriavtseva4@gmail.com>
2021-12-11 10:48:47 -0500

    .

--- barber/models.py
+++ barber/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField("Service", max_length=150)
+    description = models.TextField("Description")
+    url = models.SlugField(max_length=160, unique=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Barber(models.Model):
+    name = models.CharField(max_length=150, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
