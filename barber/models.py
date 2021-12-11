23a027e67f2a6a992cded2dcb579d9b98c6de0f4
AnastasiiaK120 <anastasiiakudriavtseva4@gmail.com>
2021-12-11 11:06:32 -0500

    image

--- barber/models.py
+++ barber/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
 class Barber(models.Model):
     name = models.CharField(max_length=150, null=True)
     description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
